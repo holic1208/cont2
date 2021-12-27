eed306c96d2a0a17a60861e56398288b987b6946
suuu <holic1208@daum.net>
2021-12-27 22:38:14 +0900

    delete

--- install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-sudo apt-get update ;
-sudo apt-get install pip -y ;
-sudo apt install awscli -y ; 
-pip3 install awscli ;
-pip3 install --upgrade awscli ;
-pip3 install ansible ;
-pip3 install boto3 ;
