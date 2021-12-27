a3cca887e0edb32035f2183a94366890f59d9527
suuu <holic1208@daum.net>
2021-12-27 22:38:38 +0900

    1

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo apt-get update ;
+sudo apt-get install pip -y ;
+sudo apt install awscli -y ; 
+pip3 install awscli ;
+pip3 install --upgrade awscli ;
+pip3 install ansible ;
+pip3 install boto3 ;
